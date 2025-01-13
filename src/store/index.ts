54767d8c6a48a1af02df02dc3340638673e49158
joseneisantana <joseneycosta@hotmail.com>
2025-01-13 16:37:48 -0300

    Finalizando projeto

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore, ReducerType } from '@reduxjs/toolkit'
+
+import carrinhoReducer from './reducers/carrinho'
+
+export const store = configureStore({
+  reducer: {
+    carrinho: carrinhoReducer
+  }
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
